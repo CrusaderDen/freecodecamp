191081cad94e3763617a171536fc892e878883d7
Denis Krestov <rupo@list.ru>
2024-01-07 00:49:17 +0300

    Step 70-75

--- Learn Basic JavaScript by Building a Role Playing Game/script.js
+++ Learn Basic JavaScript by Building a Role Playing Game/script.js
@@ -27,6 +27,12 @@ const locations = [
       "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
       "button functions": [buyHealth, buyWeapon, goTown],
       text: "You enter the store."
+   },
+   {
+      name: "cave",
+      "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+      "button functions": [fightSlime, fightBeast, goTown],
+      text: "You enter the cave. You see some monsters."
    }
 ];
 
@@ -55,10 +61,13 @@ function goStore() {
 }
 
 function goCave() {
+   update(locations[2]);
 }
 function fightDragon () {
 }
 function buyHealth () {
+   gold -= 10;
+  health += 10;
 }
 function buyWeapon () {
 }
