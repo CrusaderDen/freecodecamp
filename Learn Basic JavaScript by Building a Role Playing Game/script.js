6bf2206f2071167293a115c7cbfba9a778c4d4c8
Denis Krestov <rupo@list.ru>
2024-01-07 13:10:51 +0300

    Step 112

--- Learn Basic JavaScript by Building a Role Playing Game/script.js
+++ Learn Basic JavaScript by Building a Role Playing Game/script.js
@@ -135,8 +135,14 @@ function fightSlime () {
    goFight()
 }
 function fightBeast () {
-
+   fighting = 1
+   goFight()
 }
+
+function fightDragon() {
+   fighting = 2
+   goFight()
+ }
 
 function goFight () {
 
